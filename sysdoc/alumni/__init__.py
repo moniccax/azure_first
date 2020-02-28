1a2eb2c190894b03a3d694b1262dbea68170e5e2
monicaemediato <41020702+monicaemediato@users.noreply.github.com>
2020-02-28 09:06:12 -0300

    Update __init__.py
    cadastro init alumni

--- sysdoc/alumni/__init__.py
+++ sysdoc/alumni/__init__.py
@@ -24,10 +24,42 @@ def index():
 
 @alumni.route('/login', methods = ['GET', 'POST'])
 def login():
-    logged=current_user
+    #logged=current_user
+    if request.method == 'POST':
+        alumni_cpf=request.form['cpf']
+        passwd=request.form['password']
+        if passwd!="":
+            logged=Alumni.query.filter_by(cpf=alumni_cpf, password=hashlib.md5(passwd.encode('utf-8')))
+        #if passwd=="Alumni#Login":
+        #else
+            #logged=Alumni.query.filter_by(cpf=alumni_cpf).first()
+        if logged:
+            login_user(logged) #,remember=form.remember_me.data
+            db.session.commit()
+            session.permanent = False
+            if "remember_pass" in request.form:
+                permanent_session=request.form['remember_pass']
+                if permanent_session is not None and permanent_session:
+                    session.permanent = True
+                return redirect(url_for('alumni.index'))
+#ADD MUDAR SENHA
+        else:
+            flash('<span>CPF e/ou senha incorretos!</span>','red')
     return render_template('loginalumni.html', title='Login')
 
 @alumni.route('/cadastro', methods = ['GET', 'POST'])
 def cadastro():
-    logged=current_user
+    #logged=current_user
+    if request.method == 'POST':
+        alumni_cpf=request.form['cpf']
+        alumni_nome=request.form['nome']
+        alumni_curso_id=request.form['curso_id']
+        alumni_matricula=request.form['matricula']
+        alumni_email=request.form['email']
+        alumni_data_nascimento=request.form['data_nascimento']
+        alumni_ano_ingresso=request.form['ano_ingresso']
+        alumni_periodo_ingresso=request.form['periodo_ingresso']
+        alumni_data_colacao=request.form['data_colacao']
+        alumni_password=request.form['password']
+        #alumni_cpfRegistered=Alumni.query.filter_by(cpf=cpf).first()
     return render_template('ALUMNIcadastro.html', title='Cadastro')
